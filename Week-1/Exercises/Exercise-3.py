af76106c0ee5d88a1830bb8275f3919db546776e
louis friedland-yust <louis.friedland-yust@etu.u-paris.fr>
2025-09-11 21:39:04 +0200

    Updated answers to Exercise 3

--- Week-1/Exercises/Exercise-3.py
+++ Week-1/Exercises/Exercise-3.py
@@ -19,7 +19,7 @@ lst = [1, 4, -6, 7, 2, 3, 9, 11, 6] # In all exercises in this script, you will 
 
 print("Exercise 3.1")
 
-pass
+print(sum(lst))
 
 print("---")
 
@@ -33,7 +33,10 @@ Print the product of the elements in the list.
 
 print("Exercise 3.2")
 
-pass
+prod = 1
+for i in lst:
+    prod *= i
+print(prod)
 
 print("---")
 
@@ -47,7 +50,13 @@ Print the sum of the squares of the list.
 
 print("Exercise 3.3")
 
-pass
+import math
+
+sum = 0
+for i in lst:
+    if math.sqrt(abs(i)) % 1 == 0:
+        sum += i
+print(sum)
 
 print("---")
 
@@ -61,7 +70,7 @@ Print the largest element of the list.
 
 print("Exercise 3.4")
 
-pass
+print(max(lst))
 
 print("---")
 
@@ -75,6 +84,6 @@ Print the largest element of the list.
 
 print("Exercise 3.5")
 
-pass
+print(max(lst))
 
 print("---")
